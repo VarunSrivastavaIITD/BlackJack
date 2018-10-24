8765050f537542bce56931d25f315c86b2ae90d9
varun <varunsrivastava.v@gmail.com>
2018-10-24 17:48:24 +0530

    Fix state space representation
    Simultaneous tracking of hard and soft numbers by keeping states of the form:
    PH_PS_D_IsPair_IsIntial

--- BlackJackPlayer.py
+++ BlackJackPlayer.py
@@ -3,28 +3,30 @@ from pprint import pprint
 
 
 def create_state_space():
-    S = {'B'}
+    S = {'B', 'L', 'W', 'P'}
 
-    # Hard numbers
-    S.update(
-        'H_{}_{}'.format(i, j) for i in xrange(5, 22) for j in xrange(1, 11))
+    false = 0
+    true = 1
 
-    # Soft numbers
-    S.update(
-        'S_{}_{}'.format(i, j) for i in xrange(2, 10) for j in xrange(1, 11))
+    # Non pair non ace containing initial hands
+    S.update('{}_{}_{}_{}_{}'.format(i, i, j, false, true)
+             for i in xrange(5, 21) for j in xrange(1, 11))
 
-    # Doubled hard numbers
-    S.update(
-        'DH_{}_{}'.format(i, j) for i in xrange(5, 21) for j in xrange(1, 11))
+    # Non pair ace containing initial hands
+    S.update('{}_{}_{}_{}_{}'.format(i+1, i+11, j, false, true)
+             for i in xrange(2, 10) for j in xrange(1, 11))
 
-    # Doubled soft numbers
-    S.update(
-        'DS_{}_{}'.format(i, j) for i in xrange(2, 10) for j in xrange(1, 11))
+    # Pair containing initial hands
+    S.update('{}_{}_{}_{}_{}'.format(2*i, 2*i, j, true, true)
+             for i in xrange(1, 11) for j in xrange(1, 11))
 
-    # Split numbers
-    S.update(
-        'P_{}_{}'.format(i, j) for i in xrange(1, 11) for j in xrange(1, 11))
+    # Non pair non ace containing non initial hands
+    S.update('{}_{}_{}_{}_{}'.format(i, i, j, false, false)
+             for i in xrange(5, 22) for j in xrange(1, 11))
 
+    # Non pair ace containing non initial hands
+    S.update('{}_{}_{}_{}_{}'.format(i+1, i+11, j, false, false)
+             for i in xrange(2, 10) for j in xrange(1, 11))
     return S
 
 
